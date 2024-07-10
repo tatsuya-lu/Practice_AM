fc025f3aca6a2d0bc18f7029833b113077e34aa8
DESKTOP-RAMBKI6\USER <tatuya16228@gmail.com>
2024-07-10 13:44:04 +0900

    mountedのみasync/awaitを一時削除。動作に気になる点があったため。

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -109,18 +109,20 @@
                     }
                 }
             },
-            async mounted() {
-                await this.fetchNotifications();
+            mounted() {
+                this.fetchNotifications();
 
-                window.addEventListener('pageshow', async (event) => {
-                    if (event.persisted || (window.performance && window.performance.navigation
-                            .type === 2)) {
-                        await this.fetchNotifications();
+                // ブラウザの「戻る」ボタンでページに戻ってきた時の処理
+                window.addEventListener('pageshow', (event) => {
+                    if (event.persisted || (window.performance && window.performance.navigation.type ===
+                        2)) {
+                        this.fetchNotifications();
                     }
                 });
 
-                window.addEventListener('popstate', async () => {
-                    await this.fetchNotifications();
+                // 履歴の状態変更時の処理
+                window.addEventListener('popstate', (event) => {
+                    this.fetchNotifications();
                 });
             }
         });
